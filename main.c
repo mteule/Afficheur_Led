edc532e4792277233c34e780816c4a5ecbaec6ca
labx <labx@labx4.labx.fr>
2013-08-09 18:10:10 +0200

    Adds aff_bicolor(mtr_bicolor[NB_LIGN][NB_CASES][NB_COLOR]) !!!

--- main.c
+++ main.c
@@ -7,10 +7,12 @@
 #include "tests_affichage.h"
 #include "tests_turn_mtr.h"
 #include "tests_conv_char.h"
+#include "tests_aff_bicolor.h"
 
 void main()
 {
 	test_all_affichage();
 	test_all_turn_mtr();
 	test_all_conv_char();
+	test_all_aff_bicolor();
 }
